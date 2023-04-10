8f72531305c7a91444fa6aa4d52ccb59ee2d59c9
milkotomov <el_loco@abv.bg>
2023-04-10 18:30:26 +0300

    Team.Model.Interface1 name changed to IBoard, Board implemented IBoard

--- Tasks Management/Model/Board.cs
+++ Tasks Management/Model/Board.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Team.Model.Interface;
 
 namespace Team.Model
 {
-    public class Board
+    public class Board : Task, IBoard
     {
     }
 }
--- Tasks Management/Model/Interface/IBoard.cs
+++ Tasks Management/Model/Interface/IBoard.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Team.Model.Interface
 {
-    public interface Interface1
+    public interface IBoard
     {
     }
 }
