2fd84ac1e1497c130a95bf077da866f3a73b5650
KPalchev <kocipalchev94@gmail.com>
2023-04-11 13:06:43 +0300

    Adding prop in IFeedback

--- Tasks Management/Model/Feedback.cs
+++ Tasks Management/Model/Feedback.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Team.Model.Enum;
+using Team.Model.Interface;
 
 namespace Team.Model
 {
-    public class Feedback
+    public class Feedback : Task, IFeedback
     {
+        public Feedback(int id, string title, string description, FeedbackStatus statusType) : base(id, title, description)
+        {
+            StatusType = statusType;
+        }
+
+        public FeedbackStatus StatusType { get; }
     }
 }
--- Tasks Management/Model/Interface/IFeedback.cs
+++ Tasks Management/Model/Interface/IFeedback.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Team.Model.Enum;
 
 namespace Team.Model.Interface
 {
     public interface IFeedback
     {
-
+        FeedbackStatus StatusType { get; }
     } 
 }
