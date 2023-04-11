93ba2477dfb37fbbecd69242cbc3ee75d8d412de
milkotomov <el_loco@abv.bg>
2023-04-11 15:52:06 +0300

    Story Class Implemented

--- Tasks Management/Model/Interface/IStory.cs
+++ Tasks Management/Model/Interface/IStory.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Team.Model.Enum;
 
 namespace Team.Model.Interface
 {
-    public interface IStory
+    public interface IStory : ITask
     {
-
+        public PriorityType Priority { get; }
+        public SizeType Size { get; }
+        public StoryStatusType Status { get; }
+        public string Assignee { get; }
     }
 }
--- Tasks Management/Model/Story.cs
+++ Tasks Management/Model/Story.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Team.Model.Enum;
+using Team.Model.Interface;
+using Team.Model;
 
 namespace Team.Model
 {
-    public class Story
+    public class Story : Task, IStory
     {
+        public Story(int id, 
+                    string title, 
+                    string description, 
+                    PriorityType priority, 
+                    SizeType size, 
+                    StoryStatusType status, 
+                    string assignee)
+                    : base(id, title, description)
+        {
+            Priority = priority;
+            Size = size;
+            Status = status;
+            Assignee = assignee;
+        }
+
+        public PriorityType Priority { get; private set; }      
+
+        public SizeType Size { get; private set; }            
+
+        public StoryStatusType Status { get; private set; }
+
+        public string Assignee { get; private set; }
+
     }
 }
