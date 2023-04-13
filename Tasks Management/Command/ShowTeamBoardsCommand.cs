6504e54b7f2e69ae522df14d3ac92c51fd99c87c
mila.mitova@gmail.com <d]e=_cW)n[8-JZ>
2023-04-13 16:57:54 +0300

    Changes to ShowTeamBoardsCommand

--- Tasks Management/Command/ShowTeamBoardsCommand.cs
+++ Tasks Management/Command/ShowTeamBoardsCommand.cs
@@ -4,20 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Team.Core.Contracts;
+using Team.Exeption;
 
 namespace Team.Command
 {
     public class ShowTeamBoardsCommand : BaseCommand
     {
-        public ShowTeamBoardsCommand(IRepository repository) : base(repository)
+        public const int ExpectedNumberOfArguments = 1;
+        public ShowTeamBoardsCommand(IList<string> commandParameters, IRepository repository) : base(repository)
         {
-            Repository = repository;
         }
-
-        public IRepository Repository { get; }
         public override string Execute()
         {
-            throw new NotImplementedException();
+            if (this.CommandParameters.Count < ExpectedNumberOfArguments)
+            {
+                throw new InvalidUserInputException(@$"Invalid number of arguments. Expected: {ExpectedNumberOfArguments}, Received: {this.CommandParameters.Count}; \nPlease enter correct Team name");
+            }
+            // Parameters:
+            //  [0] - Name of the Team
+            string name = this.CommandParameters[0];
+            return ListAllBoardsOfTeam(name);
+        }
+
+        private string ListAllBoardsOfTeam(string name)
+        {
+            foreach (var team in this.Repository.Teams)
+            {
+                if (team.Name == name)
+                {
+                    if (team.Boards.Count > 0)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine($"{team.Name} boards:");
+                        foreach (var board in team.Boards)
+                        {
+                            sb.AppendLine(board.Name);
+                        }
+                        sb.Append("---------------");
+                        return sb.ToString();
+                    }
+                    else
+                    {
+                        string text = $"Team {name} doesn't have any boards.";
+                        return text;
+                    }
+                }
+                
+            }
+            string message = $"No team with name - {name} exists.";
+            return message;
         }
     }
 }
